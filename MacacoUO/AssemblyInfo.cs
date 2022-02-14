c4229e941d0cb6fbea5ad6bdd698238041f58ca4
Gabriel Slomka <gabrielslomka@gmail.com>
2022-02-14 19:04:44 +0000

    Projeto Executavel

---
+++ MacacoUO/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+#region References
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+#endregion
+
+[assembly: AssemblyDescription("Emulador de Ultima Online BR")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ MacacoUO/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MacacoUO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Core.Main(args);
+        }
+    }
+}
