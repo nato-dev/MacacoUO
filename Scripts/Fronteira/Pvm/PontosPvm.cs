17fa9ee78ce872c62c8c8bd170512a5f8326b751
Gabriel Slomka <gabrielslomka@gmail.com>
2022-02-16 08:32:49 +0000

    Exp max

--- Scripts/Fronteira/Pvm/PontosPvm.cs
+++ Scripts/Fronteira/Pvm/PontosPvm.cs
@@ -29,7 +29,7 @@ namespace Server.Ziden.Kills
         public override TextDefinition Name { get { return "Exp"; } }
         public override PointsType Loyalty { get { return PointsType.Exp; } }
         public override bool AutoAdd { get { return true; } }
-        public override double MaxPoints { get { return 2500; } }
+        public override double MaxPoints { get { return 3500; } }
         public override bool ShowOnLoyaltyGump { get { return true; } }
         public static bool Enabled = true;
     }
